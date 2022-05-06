d906b146a08c7b7e6d352a9d8c57af2e895e6d03
Nurkanat <nurkanat.doszhan@gmail.com>
2022-05-06 18:52:36 +0600

    допилить сохранение в localStorage

--- src/App.js
+++ src/App.js
@@ -1,81 +1,99 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const App = () => {
-  const [l, setL] = useState('')
-  const [tasks, setTasks] = useState([
-    { text: 'Hit the gym', completed: false },
-    { text: 'Buy eggs', completed: false },
-    { text: 'Read a book', completed: true }
-  ]);
+    let completeStyle = {
+        textDecoration: "line-through",
+        backgroundColor: "#f3f3f3"
+    };
+    let [newTask, setNewTask] = useState('')
+    let [task, setTask] = useState([
+        // { text: "Task 1", complete: true },
+        // { text: "Task 2", complete: false },
+        // { text: "Task 3", complete: false }
+    ]);
 
-  const inputChange = (e) => {
-      setL(e.target.value)
-  }
+    useEffect(() => {
+        let data = JSON.stringify(task)
+        localStorage.setItem('todos', data)
+        // setTask(data)
+        // let data = localStorage.getItem('todos')
+        // setTask(data)
+        let arr = JSON.parse(data)
+        console.log(arr)
+        // setTask(arr)
+        return () => {
+            return 0;
+        }
+    }, [task])
 
-  const InputGroup = () => {
+    let itemHandler = (i) => {
+        i.complete = !i.complete;
+        setTask([...task]);
+    };
+
+    const Item = (props) => {
+        return (
+            <li
+                className="list-group-item"
+                style={props.complete ? completeStyle : null}
+                onClick={props.itemClick}
+            >
+                {props.text}
+            </li>
+        );
+    };
+
+    const inputChange = (e) => {
+        setNewTask(e.target.value)
+    }
+
+    const addClick = () => {
+        if(newTask === '' || newTask === ' ') {
+            return 0
+        } else {
+            setTask([...task, {text: newTask, complete: false}])
+            setNewTask('')
+        }
+    }
+
     return (
-      <div className="input-group mb-3 mx-auto">
-        <input type="text"
-            value={l}
-            onChange={ inputChange }
-            className="form-control"
-            placeholder="text" />
-        <div className="input-group-append">
-          <button type="button"
-            onClick={ addNewTask }
-            className="btn btn-outline-light">Button</button>
+        <div className="App">
+            <div className="container">
+                <h1 className="pt-5 mb-3">Todo List</h1>
+                <h3 className="mb-3">Add your tasks</h3>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter your task"
+                        onChange={inputChange}
+                        value={newTask}
+                    />
+                    <button
+                        className="btn btn-outline-light"
+                        type="button"
+                        id="button-addon2"
+                        onClick={() => addClick()}
+                    >
+                        Button
+                    </button>
+                </div>
+                <ul className="list-group mt-4">
+                    {task.map((i, v) => {
+                        return (
+                            <Item
+                                key={v}
+                                text={i.text}
+                                complete={i.complete}
+                                itemClick={() => itemHandler(i)}
+                            />
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
-      </div>
-    )
-  }
-
-  const addNewTask = () => {
-      alert(l)
-    // setTasks([...tasks, {text: newTask, completed: false}])
-  }
-
-  /****** Компонент Task ******/
-  const Li = (props) => {
-    return (
-        <li
-            className="list-group-item"
-            onClick={ () => props.taskClick() }
-            style={{textDecoration: props.completed && 'line-through'}}>
-          <input type="checkbox"
-            defaultChecked={props.completed}
-          />
-          { props.text }
-        </li>
-    )
-  }
-
-  const taskClickHandler = (i) => {
-    i.completed = !i.completed
-    setTasks([...tasks])
-  }
-
-  return (
-    <div className="App">
-      <div className="container text-center">
-        <h1>My To Do List</h1>
-        <InputGroup />
-        <ul className="list-group">
-          {
-            tasks.map((i, v) => {
-              return (
-                <Li key={v}
-                    text={i.text}
-                    completed={i.completed}
-                    taskClick={() => taskClickHandler(i)}
-                />
-              )
-            })
-          }
-        </ul>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
